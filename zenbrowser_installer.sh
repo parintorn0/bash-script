ca5304857435238ea84c9d5c1687f217fc4c1d1a
parintorn0 <github@alias.toparin.me>
2025-01-22 21:26:19 +0700

    desktop entry modification added

--- zenbrowser_installer.sh
+++ zenbrowser_installer.sh
@@ -1,7 +1,6 @@
 echo "(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧  Zen Browser Installer"
 echo ""
 echo ""
-
 if [ -d /opt/zen ]; then
     echo "Let installer use root user account"
     sudo rm -R /opt/zen/
@@ -16,6 +15,42 @@ echo ""
 echo ""
 echo "Extracting..."
 sudo tar xf /tmp/zen.linux-x86_64.tar.bz2 -C /opt/
-echo "Extracted"
 sudo rm /tmp/zen.linux-x86_64.tar.bz2
+echo "Extracted"
+echo ""
+echo ""
+if [ -f /usr/share/applications/zen.desktop ]; then
+    sudo rm /usr/share/applications/zen.desktop
+    echo "Previous Desktop Entry Removed"
+    echo ""
+    echo ""
+fi
+echo "[Desktop Entry]
+Name=Zen Browser
+Comment=Experience tranquillity while browsing the web without people tracking you!
+Keywords=web;browser;internet
+Exec=/opt/zen/zen-bin %u
+Icon=/opt/zen/browser/chrome/icons/default/default128.png
+Terminal=false
+StartupNotify=true
+StartupWMClass=zen
+NoDisplay=false
+Type=Application
+MimeType=text/html;text/xml;application/xhtml+xml;application/vnd.mozilla.xul+xml;text/mml;x-scheme-handler/http;x-scheme-handler/https;
+Categories=Network;WebBrowser;
+Actions=new-window;new-private-window;profile-manager-window;
+PrefersNonDefaultGPU=true
+X-KDE-RunOnDiscreteGpu=true
+[Desktop Action new-window]
+Name=Open a New Window
+Exec=/opt/zen/zen-bin --new-window %u
+[Desktop Action new-private-window]
+Name=Open a New Private Window
+Exec=/opt/zen/zen-bin --private-window %u
+[Desktop Action profile-manager-window]
+Name=Open the Profile Manager
+Exec=/opt/zen/zen-bin --ProfileManager" >> /usr/share/applications/zen.desktop
+echo "Desktop Entry Added"
+echo ""
+echo ""
 echo "Installation Completed"
