22d9143affea15b447a41806b3df50b5e59bfe63
parintorn0 <github@alias.toparin.me>
2025-03-15 22:23:11 +0700

    update tmp file remove if exist and fix mistype bz2 to xz

--- zenbrowser_installer.sh
+++ zenbrowser_installer.sh
@@ -1,54 +1,87 @@
-echo "(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧  Zen Browser Installer"
-echo ""
-echo ""
-if [ -d /opt/zen ]; then
-    echo "Let installer use root user account"
-    sudo rm -R /opt/zen/
-    echo "Previous Installation Removed"
+install () {
+    if [ -d /opt/zen ]; then
+        echo "Let installer use root user account"
+        sudo rm -R /opt/zen/
+        echo "Previous Installation Removed"
+        echo ""
+        echo ""
+    fi
+    echo "Downloading..."
+    curl -sL "https://github.com/zen-browser/desktop/releases/latest/download/zen.linux-x86_64.tar.xz" > /tmp/zen.linux-x86_64.tar.xz
+    echo "Downloaded"
     echo ""
     echo ""
-fi
-echo "Downloading..."
-curl -sL "https://github.com/zen-browser/desktop/releases/latest/download/zen.linux-x86_64.tar.xz" > /tmp/zen.linux-x86_64.tar.bz2
-echo "Downloaded"
+    echo "Extracting..."
+    sudo tar xf /tmp/zen.linux-x86_64.tar.xz -C /opt/
+    if [ -e /tmp/zen.linux-x86_64.tar.xz ]; then
+    	sudo rm /tmp/zen.linux-x86_64.tar.xz
+    fi
+    echo "Extracted"
+    echo ""
+    echo ""
+    if [ -f /usr/share/applications/zen.desktop ]; then
+        sudo rm /usr/share/applications/zen.desktop
+        echo "Previous Desktop Entry Removed"
+    fi
+    echo "[Desktop Entry]
+    Name=Zen Browser
+    Comment=Experience tranquillity while browsing the web without people tracking you!
+    Keywords=web;browser;internet
+    Exec=/opt/zen/zen-bin %u
+    Icon=/opt/zen/browser/chrome/icons/default/default128.png
+    Terminal=false
+    StartupNotify=true
+    StartupWMClass=zen
+    NoDisplay=false
+    Type=Application
+    MimeType=text/html;text/xml;application/xhtml+xml;application/vnd.mozilla.xul+xml;text/mml;x-scheme-handler/http;x-scheme-handler/https;
+    Categories=Network;WebBrowser;
+    Actions=new-window;new-private-window;profile-manager-window;
+    [Desktop Action new-window]
+    Name=Open a New Window
+    Exec=/opt/zen/zen-bin --new-window %u
+    [Desktop Action new-private-window]
+    Name=Open a New Private Window
+    Exec=/opt/zen/zen-bin --private-window %u
+    [Desktop Action profile-manager-window]
+    Name=Open the Profile Manager
+    Exec=/opt/zen/zen-bin --ProfileManager" >> /tmp/zen.desktop
+    sudo mv /tmp/zen.desktop /usr/share/applications/zen.desktop
+    sudo rm /tmp/zen.desktop
+    echo "Desktop Entry Added"
+    echo ""
+    echo ""
+    echo "Installation Completed"
+}
+
+uninstall () {
+    if [ -d /opt/zen ]; then
+        echo "Let uninstaller use root user account"
+        sudo rm -R /opt/zen/
+        echo "Uninstalled"
+        echo ""
+        echo ""
+        if [ -f /usr/share/applications/zen.desktop ]; then
+            sudo rm /usr/share/applications/zen.desktop
+            echo "Desktop Entry Removed"
+        fi
+        echo ""
+        echo ""
+        echo "Uninstallation Conpleted"
+    fi
+}
+
+
+echo "Zen Browser Installer"
 echo ""
 echo ""
-echo "Extracting..."
-sudo tar xf /tmp/zen.linux-x86_64.tar.bz2 -C /opt/
-sudo rm /tmp/zen.linux-x86_64.tar.bz2
-echo "Extracted"
-echo ""
-echo ""
-if [ -f /usr/share/applications/zen.desktop ]; then
-    sudo rm /usr/share/applications/zen.desktop
-    echo "Previous Desktop Entry Removed"
-fi
-echo "[Desktop Entry]
-Name=Zen Browser
-Comment=Experience tranquillity while browsing the web without people tracking you!
-Keywords=web;browser;internet
-Exec=/opt/zen/zen-bin %u
-Icon=/opt/zen/browser/chrome/icons/default/default128.png
-Terminal=false
-StartupNotify=true
-StartupWMClass=zen
-NoDisplay=false
-Type=Application
-MimeType=text/html;text/xml;application/xhtml+xml;application/vnd.mozilla.xul+xml;text/mml;x-scheme-handler/http;x-scheme-handler/https;
-Categories=Network;WebBrowser;
-Actions=new-window;new-private-window;profile-manager-window;
-[Desktop Action new-window]
-Name=Open a New Window
-Exec=/opt/zen/zen-bin --new-window %u
-[Desktop Action new-private-window]
-Name=Open a New Private Window
-Exec=/opt/zen/zen-bin --private-window %u
-[Desktop Action profile-manager-window]
-Name=Open the Profile Manager
-Exec=/opt/zen/zen-bin --ProfileManager" >> /tmp/zen.desktop
-sudo mv /tmp/zen.desktop /usr/share/applications/zen.desktop
-sudo rm /tmp/zen.desktop
-echo "Desktop Entry Added"
-echo ""
-echo ""
-echo "Installation Completed"
+read -p "install or remove (i, r): " charInput
+case $charInput in
+    I | i)
+        install
+        ;;
+    R | r)
+        uninstall
+        ;;
+esac
+
